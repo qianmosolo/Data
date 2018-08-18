a65ec094e9d413a4657050f363b6ba5c9cfce34b
chim <qianmosolo@163.com>
2018-08-18 15:51:06 +0800

    recommend

--- filterdata.py
+++ filterdata.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+import codecs
 from math import sqrt
 
 users = {"Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0, "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0},
@@ -112,3 +113,167 @@ def cosineSimilarity(rating1, rating2):
 
 # print cosineSimilarity(users['Veronica'], users['Angelica'])
 # print cosineSimilarity(Clara, Robert)
+
+
+class recommender:
+	def __init__(self, data, k=1, method='pearson', n=5):
+		self.k = k
+		self.n = n
+		self.method = method
+		self.productid2name = {}
+		self.userid2name = {}
+		self.username2id = {}
+
+
+		if isinstance(data, dict):
+			self.data = data
+
+
+	def loadData(self, path=''):
+		self.data = {}
+		i = 0
+		f = codecs.open(path + 'BX-Book-Ratings.csv', 'r', 'utf8')
+		for line in f:
+			i += 1
+			fields = line.split(';')
+			userid = fields[0].strip('"')
+			bookid = fields[1].strip('"')
+			rating = int(fields[2].strip().strip('"'))
+			if userid in self.data:
+				currentRatings = self.data[userid]
+			else:
+				currentRatings = {}
+			currentRatings[bookid] = rating
+			self.data[userid] = currentRatings
+		f.close()
+
+		f = codecs.open(path + 'BX-Books.csv', 'r', 'utf8')
+		for line in f:
+			i += 1
+			fields = line.split(';')
+			isbn = fields[0].strip('"')
+			title = fields[1].strip('"')
+			author = fields[2].strip('"').strip()
+			title = title + ' by ' + author
+			self.productid2name[isbn] = title
+		f.close()
+
+		f = codecs.open(path + 'BX-Users.csv', 'r', 'utf8')
+		for line in f:
+			i += 1
+			fields = line.split(';')
+			userid = fields[0].strip('"')
+			location = fields[1].strip('"')
+			if len(fields) > 2:
+				age = fields[2].strip().strip('"')
+			else:
+				age = 'NULL'
+			if age != 'NULL':
+				value = location + ' (age: ' + age + ')'
+			else:
+				value = location
+			self.userid2name[userid] = value
+			self.username2id[location] = userid
+		f.close()
+		print(i)
+
+	def getProductNameById(self, id):
+		return self.productid2name.get(id, id)
+
+	def userRatings(self, id, n):
+		print 'Ratings for ' + self.userid2name[id]
+		ratings = self.data[id]
+		print len(ratings)
+		ratings = list(ratings.items)
+		ratings = [(self.getProductNameById(k), v)
+					for (k, v) in ratings]
+		ratings.sort(key=lambda x: x[1], reverse=True)
+
+		ratings = ratings[:n]
+		for rating in ratings:
+			print '%s\t%i' % (rating[0], rating[1])
+
+	
+	def pearson(self, rating1, rating2):
+		'''
+		皮尔逊
+		'''
+		sum_xy = 0
+		sum_x, sum_y = 0, 0
+		sum_x2, sum_y2 = 0, 0
+		n = 0
+		for key in rating1:
+			if key in rating2:
+				n += 1
+				x = rating1[key] 
+				y = rating2[key] 
+				sum_xy += x * y
+				sum_x += x
+				sum_y += y
+				sum_x2 += pow(x, 2)
+				sum_y2 += pow(y, 2)
+		if n == 0:
+			return 0 
+		# 分母
+		denominator = sqrt(sum_x2 - pow(sum_x, 2) / n) * sqrt(sum_y2 - pow(sum_y, 2) / n)
+		if denominator == 0:
+			return 0
+		else:
+			return (sum_xy - (sum_x * sum_y) / n) / denominator
+
+
+	def computerNearestNeighbor(self, username):
+		'''
+		寻找距离最近的用户
+		'''
+		distances = []
+		# if username not in users:
+		# 	raise KeyError(username + ' does not exist')
+		for user in self.data:
+			if user != username:
+				distance = self.pearson(self.data[username], self.data[user])
+				distances.append((user, distance))
+		# pearson相关系数越大,越相似
+		distances.sort(key=lambda x: x[1], reverse=True)
+		return distances
+
+	# print computerNearestNeighbor('Chan', users, manhattan)
+
+	def recommend(self, user):
+		'''推荐'''
+		nearest = self.computerNearestNeighbor(user)
+		recommendations = {}
+
+		userItems = self.data[user]
+
+		totalDistance = 0.0
+		for i in range(self.k):
+			totalDistance += nearest[i][1]
+
+		for i in range(self.k):
+			weight = nearest[i][1] / totalDistance
+			name = nearest[i][0]
+			neighborItems = self.data[name]
+			for artist in neighborItems:
+				if artist not in userItems:
+					if artist not in recommendations:
+						recommendations[artist] = (neighborItems[artist] * weight)
+					else:
+						recommendations[artist] = (neighborItems[artist] + 
+													neighborItems[artist] * weight)
+		
+		recommendations = list(recommendations.items())
+		recommendations = [(self.getProductNameById(k), v)
+							for (k, v) in recommendations]
+		recommendations.sort(key=lambda x: x[1], reverse=True)
+		
+		return recommendations[:self.n]
+
+
+
+r = recommender(users)
+# print r.recommend('Jordyn')
+# print r.recommend('Hailey')
+
+r.loadData('./BX-Dump/')
+print r.recommend(u'171118')
